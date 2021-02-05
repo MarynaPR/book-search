bbb155e0c1b8f113a059ee651a459a772ee99507
MarynaPR <pryadkamaryna@gmail.com>
2021-02-04 23:19:24 -0600

    server file updated with mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await (await User.findOne({ _id: context.user._id })).isSelected('-__v -password');
+
+                return userData;
+            }
+            throw new AuthenticationError('you are not logged in');
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: {},
+        removeBook: {}
+    },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
